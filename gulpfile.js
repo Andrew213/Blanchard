5c0e7737fe74bdb5fd50d6f7e9871f0dd183c2cb
unknown <mega.zip2011@gmail.com>
2021-02-06 18:38:15 +0300

    fixed gallery select and slider

--- gulpfile.js
+++ gulpfile.js
@@ -68,7 +68,7 @@ function html() {
 	return src(path.src.html, {})
 		.pipe(plumber())
 		.pipe(fileinclude())
-		.pipe(webphtml())
+		// .pipe(webphtml())
 		.pipe(dest(path.build.html))
 		.pipe(browsersync.stream());
 }
